89ef057be2ec59e957156486067a59dd40b91806
Mousa Najafi <monajafi@gmail.com>
2019-07-15 08:46:29 +0430

    animation added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(BouncingBallApp());
@@ -7,12 +9,34 @@ class BouncingBallApp extends StatefulWidget {
   _BouncingBallAppState createState() => _BouncingBallAppState();
 }
 
-class _BouncingBallAppState extends State<BouncingBallApp> {
+class _BouncingBallAppState extends State<BouncingBallApp> with TickerProviderStateMixin {
+  AnimationController controller;
+  Animation<Point> animation;
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(duration: const Duration(seconds: 5),vsync: this);
+    animation = Tween<Point>(begin: Point(0, 0),end: Point(200, 200)).animate(controller)
+                ..addStatusListener((status){
+                  if(status == )
+                });
+    controller.forward();
+  }
   @override
   Widget build(BuildContext context) {
+    return AnimatedBall(animation: animation,);
+  }
+}
+
+class AnimatedBall extends AnimatedWidget{
+  AnimatedBall({Key key,Animation animation}):
+              super(key:key,listenable:animation);
+  @override
+  Widget build(BuildContext context) {
+    final Animation<Point> animation = listenable;
     return Center(
       child: Transform.translate(
-        offset: const Offset(20, 100),
+        offset:  Offset(animation.value.x.toDouble(), animation.value.y.toDouble()),
         child: Container(
           width: 100,
           height: 100,
@@ -20,6 +44,7 @@ class _BouncingBallAppState extends State<BouncingBallApp> {
               BoxDecoration(color: Colors.orange, shape: BoxShape.circle),
         ),
       ),
-    );
+    );;
   }
+
 }
